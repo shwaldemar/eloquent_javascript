6ca864d1361e40d5d0f4c370a4d8831be6779764
Simon <shwaldemar@gmail.com>
2019-11-05 19:28:28 +0000

    vector exercise completed

--- exercises/vector_type.js
+++ exercises/vector_type.js
@@ -1,24 +1,24 @@
 //Write a constructor Vector that represents a vector in two-dimensional space. It takes x and y parameters (numbers), which it should save to properties of the same name.
 //Give the Vector prototype two methods, plus and minus, that take another vector as a parameter and return a new vector that has the sum or difference of the two vectors’ (the one in this and the parameter) x and y values.
-function Vector(x,y){
+
+function Vector(x, y) {
   this.x = x;
   this.y = y;
 }
 
-Vector.prototype.plus = (vector) => {
-  return new Vector(this.x + vector.x, this.y + vector.y);
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
 };
 
-Vector.prototype.minus = (vector) => {
-  return new Vector(this.x - vector.x, this.y - vector.y)
-}
+Vector.prototype.minus = function(other) {
+  return new Vector(this.x - other.x, this.y - other.y);
+};
 
 Object.defineProperty(Vector.prototype, "length", {
-  get: function(){
-    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  get: function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
   }
 });
-
 
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
